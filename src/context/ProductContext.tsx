487533e28762c1b6f255ba8a65a1af5653207c25
Leandro Reis <leandro.e.reis@gmail.com>
2020-09-15 19:08:45 -0300

    ajuste de contexto

--- src/context/ProductContext.tsx
+++ src/context/ProductContext.tsx
@@ -1,6 +1,5 @@
 import { useContext } from 'react';
 
-import { Product } from '../types/product';
 import { ProductListContext } from './ProductListContext';
 import { useParams } from 'react-router-dom';
 
@@ -22,7 +21,7 @@ import { useParams } from 'react-router-dom';
 export function useProduct() {
   const context = useContext(ProductListContext);
 
-  const { id } = useParams();
+  const id = useParams();
   const data = context.data.filter((product) => product.id === id)[0];
   return { data };
 }
